fbe4ed65cb8a2bf9f089136e4ca62dc0a23bf860
BoyangChen <qinyuhua@Yuhuas-MacBook-Air.local>
2014-06-17 23:26:57 +0800

    improved the geometry module, try to use plain english to name quantities. it's longer, but easier to read and less likely to make mistakes

--- modgeo.f
+++ modgeo.f
@@ -1,47 +1,138 @@
-      module modgeo
-      use modparam
+      module geometrymodule
+      use parametermodule
 !-------------------------------------------------------     
 !---- module for geometrical quantities ----------------
 !-------------------------------------------------------
       
       implicit none
       
+      integer,parameter :: namelength=10 ! length for the name of the geo. quantity
+      
       type point
-        integer :: nmber
-        real(kind=dp) :: x, y, z
+        real(kind=dp) :: x(3)
       end type point
       
       type edge
-        integer :: nmber
-        type(point) :: pnt1, pnt2
-        !integer :: dir ! dir=0 if edge vector is from pnt1 to pnt2; dir=1 if otherwise
+        type(point) :: points(2)
       end type edge
       
-      type tri
-        type(point) :: pnt1, pnt2, pnt3
-        type(edge) :: edg1, edg2, edg3
-        !integer :: dir ! dir=0 if positive permutation; dir=1 if otherwise
-      end type tri
+      type surface
+        character(len=namelength) :: itstype
+        type(point),dimension(:),allocatable :: points
+        type(edge),dimension(:),allocatable :: edges    
+      end type surface
       
-      type quad
-        type(point) :: pnt1, pnt2, pnt3, pnt4
-        type(edge) :: edg1, edg2, edg3, edg4
-        !integer :: dir ! dir=0 if positive permutation; dir=1 if otherwise        
-      end type quad
+	  type volume
+        character(len=namelength) :: itstype
+        type(point),dimension(:),allocatable :: points
+        type(edge),dimension(:),allocatable :: edges
+        type(surface),dimension(:),allocatable :: surfaces
+	  end type volume
       
-      type tet
       
-      end type tet
       
-      type wedge
-      
-      end type wedge
-      
-      type hex
-      
-      end type hex
       
       contains
       
       
-      end module modgeo
+      
+      
+      subroutine initializepoint(thispoint)
+      
+      	type(point) :: thispoint
+      	
+      	thispoint%x(:)=zero
+      
+      end subroutine intializepoint
+      
+      
+      
+      subroutine initializeedge(thisedge)
+      
+      	type(edge) :: thisedge
+      	
+      	integer :: i
+      	
+      	i=0
+      	
+      	do i=1,2
+      		call initializepoint(thisedge%point(i))
+        enddo
+      
+      end subroutine intializeedge
+      
+      
+      
+      
+      subroutine initializesurface(thissurface,itstype)
+      
+      	type(surface) :: thissurface
+      	character(len=namelength) :: itstype
+      	
+      	integer :: numberofpoints, numberofedges,i
+      	
+      	numberofpoints=0
+      	numberofedges=0
+      	i=0
+      	
+      	thissurface%itstype=itstype
+      	
+      	select case(itstype)
+      	
+      		case('triangle')
+      			numberofpoints=3
+      			numberofedges=3
+      			
+      		case('quadrilateral')
+      			numberofpoints=4
+      			numberofedges=4
+      			
+      		case default
+      		! write some msg on msg file, then exit
+      			return
+      			
+        end select
+        
+        allocate(thissurface%points(numberofpoints),thissurface%edges(numberofedges))
+        ! check success
+        
+      	do i=1,numberofpoints
+      		call initialize(thissurface%points(i))
+      	enddo
+      	
+      	do i=1,numberofedges
+      		call initialize(thissurface%edges(i))
+      	enddo  
+      
+      return
+      end subroutine intializesurface
+      
+      
+      
+      
+      subroutine initializevolume(thisvolume,itstype)
+
+      	type(volume) :: thisvolume
+      	character(len=namelength) :: itstype
+      	
+      	thisvolume%itstype=itstype
+      	
+      	select case(itstype)
+      	
+      		case('tetrahedron')
+      		
+      		case('wedge')
+      		
+      		case('brick')
+      		
+      		case default
+      		
+        end select      
+      
+      return
+      end subroutine intializevolume
+      
+      
+      
+      
+      end module geometrymodule
