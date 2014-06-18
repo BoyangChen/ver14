be33b1825fef365b19619550ff672c53e7bad3f3
BoyangChen <boyang.chen.cn@gmail.com>
2014-06-18 20:35:27 +0800

    geometry module
    - included prepare and build (renamed the previous 'place') procedures
    in the type definition
    - hide all internal procedures of the module

--- geometry_module.f
+++ geometry_module.f
@@ -5,48 +5,69 @@
 !-------------------------------------------------------
       
       implicit none
+      private
       
-      !integer, parameter :: name_length=10 ! length for the name of the geo. quantity
       
       !---- define geometrical quantities ------
-      type point
+      type, public :: point
         real(kind=dp) :: x(3)
+        contains
+          procedure :: prepare => prepare_point
+          procedure :: build => build_point
       end type point
       
-      type edge
+      type, public :: edge
         type(point) :: points(2)
+        contains
+          procedure :: prepare => prepare_edge
+          procedure :: build => build_edge
       end type edge
       
-      type triangle
+      type, public :: triangle
         type(point) :: points(3)
         type(edge) :: edges(3)  
+        contains
+          procedure :: prepare => prepare_triangle
+          procedure :: build => build_triangle
       end type triangle
       
-      type quadrilateral
+      type, public :: quadrilateral
         type(point) :: points(4)
         type(edge) :: edges(4)  
+        contains
+          procedure :: prepare => prepare_quadrilateral
+          procedure :: build => build_quadrilateral
       end type quadrilateral
       
-      type tetrahedron
+      type, public :: tetrahedron
         type(point) :: points(4)
         type(edge) :: edges(6)
         type(triangle) :: triangles(4)
+        contains
+          procedure :: prepare => prepare_tetrahedron
+          procedure :: build => build_tetrahedron
       end type tetrahedron
       
-      type wedge
+      type, public :: wedge
         type(point) :: points(6)
         type(edge) :: edges(9)
         type(triangle) :: triangles(2)
         type(quadrilateral) :: quadrilaterals(3)
+        contains
+          procedure :: prepare => prepare_wedge
+          procedure :: build => build_wedge
       end type wedge
       
-      type brick
+      type, public :: brick
         type(point) :: points(8)
         type(edge) :: edges(12)
         type(quadrilateral) :: quadrilaterals(6)
+        contains
+          procedure :: prepare => prepare_brick
+          procedure :: build => build_brick
       end type brick
       
-      !-----------------------------------------
+
       
       
       
@@ -58,7 +79,7 @@
       ! prepare a point with zero coordinates
       subroutine prepare_point(this_point)
       
-      	type(point),intent(inout) :: this_point
+      	class(point),intent(inout) :: this_point
       	
       	this_point%x(:)=zero
       
@@ -68,7 +89,7 @@
       ! prepare an edge
       subroutine prepare_edge(this_edge)
       
-      	type(edge),intent(inout) :: this_edge
+      	class(edge),intent(inout) :: this_edge
       	
       	integer :: i
       	
@@ -84,7 +105,7 @@
       ! prepare a triangle
       subroutine prepare_triangle(this_surface)
       
-        type(triangle),intent(inout) :: this_surface
+        class(triangle),intent(inout) :: this_surface
         
       	integer :: i
       	
@@ -105,7 +126,7 @@
       ! prepare a quadrilateral
       subroutine prepare_quadrilateral(this_surface)
       
-        type(quadrilateral),intent(inout) :: this_surface
+        class(quadrilateral),intent(inout) :: this_surface
         
       	integer :: i
       	
@@ -126,7 +147,7 @@
       ! prepare a tetrahedron
       subroutine prepare_tetrahedron(this_volume)
       
-        type(tetrahedron),intent(inout) :: this_volume
+        class(tetrahedron),intent(inout) :: this_volume
         
       	integer :: i
       	
@@ -151,7 +172,7 @@
       ! prepare a wedge
       subroutine prepare_wedge(this_volume)
       
-        type(wedge),intent(inout) :: this_volume
+        class(wedge),intent(inout) :: this_volume
         
       	integer :: i
       	
@@ -180,7 +201,7 @@
       ! prepare a brick
       subroutine prepare_brick(this_volume)
       
-        type(brick),intent(inout) :: this_volume
+        class(brick),intent(inout) :: this_volume
         
       	integer :: i
       	
@@ -203,40 +224,40 @@
  
  
  
-      ! place a point on desired coordinates
-      subroutine place_point(this_point,x)
+      ! build a point on desired coordinates
+      subroutine build_point(this_point,x)
       
-      	type(point),intent(inout) :: this_point
+      	class(point),intent(inout) :: this_point
         real(kind=dp),intent(in) :: x(:)
       	
         if(size(this_point%x)==size(x)) then
             this_point%x(:)=x(:)
         else
-            stop "dimension mismatch in place_point"
+            stop "dimension mismatch in build_point"
         end if
       
-      end subroutine place_point 
+      end subroutine build_point 
       
       
-      ! place an edge on desired locations
-      subroutine place_edge(this_edge,points)
+      ! build an edge on desired locations
+      subroutine build_edge(this_edge,points)
       
-      	type(edge),intent(inout) :: this_edge
+      	class(edge),intent(inout) :: this_edge
         type(point),intent(in) :: points(:)
       	
         if(size(this_edge%points)==size(points)) then    
             this_edge%points(:)=points(:)
         else
-            stop "dimension mismatch in place_edge"
+            stop "dimension mismatch in build_edge"
         end if
       
-      end subroutine place_edge
+      end subroutine build_edge
   
   
-      ! place a triangle
-      subroutine place_triangle(this_surface,edges,points)
+      ! build a triangle
+      subroutine build_triangle(this_surface,edges,points)
       
-        type(triangle),intent(inout) :: this_surface
+        class(triangle),intent(inout) :: this_surface
         type(edge),intent(in) :: edges(:)
         type(point),intent(in) :: points(:)
         
@@ -245,16 +266,16 @@
             this_surface%edges(:)=edges(:)
             this_surface%points(:)=points(:)
         else
-            stop "dimension mismatch in place_triangle"
+            stop "dimension mismatch in build_triangle"
         end if
       
-      end subroutine place_triangle
+      end subroutine build_triangle
       
 
-      ! place a quadrilateral
-      subroutine place_quadrilateral(this_surface,edges,points)
+      ! build a quadrilateral
+      subroutine build_quadrilateral(this_surface,edges,points)
       
-        type(quadrilateral),intent(inout) :: this_surface
+        class(quadrilateral),intent(inout) :: this_surface
         type(edge),intent(in) :: edges(:)
         type(point),intent(in) :: points(:)
         
@@ -263,16 +284,16 @@
             this_surface%edges(:)=edges(:)
             this_surface%points(:)=points(:)
         else
-            stop "dimension mismatch in place_quadrilateral"
+            stop "dimension mismatch in build_quadrilateral"
         end if
       
-      end subroutine place_quadrilateral
+      end subroutine build_quadrilateral
       
       
-      ! place a tetrahedron
-      subroutine place_tetrahedron(this_volume,triangles,edges,points)
+      ! build a tetrahedron
+      subroutine build_tetrahedron(this_volume,triangles,edges,points)
       
-        type(tetrahedron),intent(inout) :: this_volume
+        class(tetrahedron),intent(inout) :: this_volume
         type(triangle),intent(in) :: triangles(:)
         type(edge),intent(in) :: edges(:)
         type(point),intent(in) :: points(:)
@@ -284,17 +305,17 @@
             this_volume%edges(:)=edges(:)
             this_volume%points(:)=points(:)
         else
-            stop "dimension mismatch in place_tetrahedron"
+            stop "dimension mismatch in build_tetrahedron"
         end if
       
-      end subroutine place_tetrahedron   
+      end subroutine build_tetrahedron   
       
       
-      ! place a wedge
-      subroutine place_wedge(this_volume,quadrilaterals,triangles, & 
+      ! build a wedge
+      subroutine build_wedge(this_volume,quadrilaterals,triangles, & 
       edges,points)
       
-        type(wedge),intent(inout) :: this_volume
+        class(wedge),intent(inout) :: this_volume
         type(quadrilateral),intent(in) :: quadrilaterals(:)
         type(triangle),intent(in) :: triangles(:)
         type(edge),intent(in) :: edges(:)
@@ -309,17 +330,17 @@
             this_volume%edges(:)=edges(:)
             this_volume%points(:)=points(:)
         else
-            stop "dimension mismatch in place_wedge"
+            stop "dimension mismatch in build_wedge"
         end if
       
-      end subroutine place_wedge
+      end subroutine build_wedge
       
       
-      ! place a brick
-      subroutine place_brick(this_volume,quadrilaterals, & 
+      ! build a brick
+      subroutine build_brick(this_volume,quadrilaterals, & 
       edges,points)
       
-        type(wedge),intent(inout) :: this_volume
+        class(wedge),intent(inout) :: this_volume
         type(quadrilateral),intent(in) :: quadrilaterals(:)
         type(edge),intent(in) :: edges(:)
         type(point),intent(in) :: points(:)
@@ -331,10 +352,10 @@
             this_volume%edges(:)=edges(:)
             this_volume%points(:)=points(:)
         else
-            stop "dimension mismatch in place_brick"
+            stop "dimension mismatch in build_brick"
         end if
       
-      end subroutine place_brick
+      end subroutine build_brick
       
       
       
