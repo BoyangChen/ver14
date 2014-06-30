0bd533691ec078f5dd76c90abfd9fe5e68f120f0
BoyangChen <boyang.chen.cn@gmail.com>
2014-06-30 12:08:04 +0800

    tri element module
    further improved the tri element module
    -removed the glb stress and strains in ig points
    - removed the stress and strain terms in element
    - added k matrix and f vector in the integration subroutine local
    variables, so element k and f are updated at the end

--- fnm/tri_element_module.f90
+++ fnm/tri_element_module.f90
@@ -9,30 +9,45 @@
     
     integer,parameter :: ndim=2, nst=3, nnode=3, nig=1, ndof=ndim*nnode ! constants for type tri_element
     
-    type :: tri_ig_point ! everything needed for storage/output at ig point
+    
+    
+    
+    type :: tri_ig_point ! private, everything needed for storage/output at ig point
+    
         real(kind=dp) :: x(ndim) ! physical coordinates
-        real(kind=dp) :: glb_stress(nst), lcl_stress(nst) ! stresses for output
-        real(kind=dp) :: glb_strain(nst), lcl_strain(nst) ! strains for output
+        real(kind=dp) :: stress(nst) ! stresses for output
+        real(kind=dp) :: strain(nst) ! strains for output
+        
+        ! below are optional terms 
+        
         real(kind=dp),allocatable :: rsdv(:) ! sdvs for calculation and output
         integer,allocatable :: isdv(:)
         logical,allocatable :: lsdv(:)
+        
     end type :: tri_ig_point
-    ! use update procedure to fill in x first, then after calculation, fill in the rest
+
+    
     
     
     type, public :: tri_element 
         private
+        
         integer :: connec(nnode) ! node indices in their global arrays
         integer :: matkey ! material index in the global material arrays
         type(tri_ig_point) :: ig_point(nig) ! x, xi, weight, stress, strain, sdv
         real(kind=dp) :: K_matrix(ndof,ndof), F_vector(ndof) ! k matrix and f vector
-        real(kind=dp) :: glb_stress(nst), lcl_stress(nst)
-        real(kind=dp) :: glb_strain(nst), lcl_strain(nst)
         logical :: PlaneStrain
+        
+        ! below are optional terms 
+        
         real(kind=dp),allocatable :: rsdv(:)
         integer,allocatable :: isdv(:)
         logical,allocatable :: lsdv(:)
+        
     end type
+    
+    
+    
     
     interface empty
         module procedure empty_tri_element
@@ -45,6 +60,8 @@
     interface integrate
         module procedure integrate_tri_element
     end interface
+    
+    
 
 
     public :: empty,update,integrate
@@ -55,9 +72,10 @@
     contains
     
     
-    
-    
+    ! this subroutine is used to format the element for use
+    ! it is used in the initialize_lib_elem procedure in the lib_elem module
     subroutine empty_tri_element(elem)
+    
         type(tri_element),intent(out) ::elem
         
         integer :: i
@@ -65,36 +83,25 @@
         
         elem%connec=0
         elem%matkey=0
-        
         do i=1:nig
             call empty_ig(elem%ig_point(i))
         end do
-        
-        elem%K_matrix=zero
-        elem%F_vector=zero
-        elem%glb_stress=zero
-        elem%lcl_stress=zero
-        elem%glb_strain=zero
-        elem%lcl_strain=zero
-        
-        elem%PlaneStrain=.false. ! default value
-        
-        if(allocated(elem%avg_rsdv)) then
-            deallocate(elem%avg_rsdv)
-        end if
-        
-        if(allocated(elem%avg_isdv)) then
-            deallocate(elem%avg_isdv)
-        end if
-        
-        if(allocated(elem%avg_lsdv)) then
-            deallocate(elem%avg_lsdv)
-        end if
+        elem%K_matrix=zero   
+        elem%F_vector=zero       
+        elem%PlaneStrain=.false. ! default value      
+        if(allocated(elem%avg_rsdv)) deallocate(elem%avg_rsdv)       
+        if(allocated(elem%avg_isdv)) deallocate(elem%avg_isdv)
+        if(allocated(elem%avg_lsdv)) deallocate(elem%avg_lsdv)
     
     end subroutine empty_tri_element
     
     
+    
+    
+    ! this subroutine is used to fill in the connectivity and material lib index of the element
+    ! it is used in the initialize_lib_elem procedure in the lib_elem module
     subroutine update_tri_element(elem,connec,matkey)
+    
         type(tri_element),intent(inout) :: elem
         integer,intent(in) :: connec(nnode)
         integer,intent(in) :: matkey
@@ -107,6 +114,9 @@
     
     
     
+    
+    ! the integration subroutine, updates K matrix, F vector, integration point stress and strain
+    ! as well as all the solution dependent variables (sdvs) at intg points and element
     subroutine integrate_tri_element(elem)
     
         type(tri_element),intent(inout) :: elem  
@@ -118,16 +128,17 @@
         type(material) :: mat ! matname, mattype and matkey to glb mattype array
         
         ! variables defined locally
+        real(kind=dp) :: K_matrix(ndof,ndof), F_vector(ndof)
         real(kind=dp) :: igxi(ndim,nig),igwt(nig) ! ig point natural coords and weights
         real(kind=dp) :: coords(ndim,nnode) ! coordinates of the element nodes
         real(kind=dp) :: u(ndof) ! nodal disp. vector
+        logical :: PlaneStrain ! as the name suggests, true for plane strain stiffness
         real(kind=dp) :: fn(nnode),dn(nnode,ndim) ! shape functions & their deriv. physical space
         real(kind=dp) :: jac(ndim,ndim),gn(nnode,ndim),detj ! jacobian & shape func. deriv. natural space
         real(kind=dp) :: bee(nst,ndof),beet(ndof,nst) ! b matrix and its transpose
         real(kind=dp) :: dee(nst,nst),deeg(nst,nst) ! d matrix in local & global coord. syst.
         real(kind=dp) :: btd(ndof,nst),btdb(ndof,ndof) ! b'*d & b'*d*b
         real(kind=dp) :: tmpstrain(nst),tmpstress(nst) ! temporary strain and stress arrays
-        logical :: PlaneStrain ! as the name suggests, true for plane strain stiffness
         real(kind=dp),allocatable :: tmprsdv(:) ! temporary real sdv arrays
         integer,allocatable :: tmpisdv(:)
         logical,allocatable :: tmplsdv(:)
@@ -140,8 +151,10 @@
             call empty(node(i))
         end do
         call empty(mat)
+        K_matrix=zero; F_vector=zero
         igxi=zero; igwt=zero
-        coords=zero; u=zero     
+        coords=zero; u=zero
+        PlainStrain=.false.
         
         ! copy nodes from global node array 
         node(:)=lib_node(elem%connec(:))
@@ -155,14 +168,18 @@
         ! copy material values from global material array
         mat=lib_mat(elm%matkey)
         
+        ! copy plain strain variable from element
+        PlainStrain=elem%PlainStrain
         
         ! update ig point xi and weight
         call tri_ig(igxi,igwt)
+          
+        
         
         !-calculate strain,stress,stiffness,sdv etc. at each int point
       	do kig=1,nig 
         
-            ! empty relevant arrays
+            ! empty relevant arrays for reuse
             fn=zero; dn=zero
             jac=zero; gn=zero; detj=zero
             bee=zero; beet=zero; dee=zero; deeg=zero
@@ -173,17 +190,12 @@
             !~if(allocated(tmplsdv)) tmplsdv=.false.
         
             call tri_shape(igxi,fn,dn,kig) !-to get shape matrix and derivatives
-            
-            jac=matmul(coords,dn) ! get jacobian
-            
-     		call determinant(jac,detj,ndim) !-get det(jac)
-            
-     		call jac_inv(jac,detj,ndim) ! invert jac onto itself
-            
-            gn=matmul(dn,jac)
-            
+            jac=matmul(coords,dn) ! get jacobian           
+     		call determinant(jac,detj,ndim) !-get det(jac)            
+     		call jac_inv(jac,detj,ndim) ! invert jac onto itself           
+            gn=matmul(dn,jac)           
      		call beemat(bee,gn,nnode,nst,ndof) !-calculate b matrix nst*ndof
-
+            
             ! calculate global strains
         	do i=1,nst
                 do j=1,ndof
@@ -191,14 +203,9 @@
                 end do
         	end do
             
-            ! update global strains to ig point glb_strain array
-            call update_ig(elem%ig_point(kig),glb_strain=tmpstrain)
-
             ! transfer strain to local coordinates
         	call ktransfer_strain(nst,tmpstrain,theta) !-strain now in local coords
             
-            ! update local strains to ig point lcl_strain array
-            call update_ig(elem%ig_point(kig),lcl_strain=tmpstrain)
             
             ! update temp sdvs from elem ig point sdvs
             !..........Not included at the moment..........
@@ -208,18 +215,12 @@
                 case('isotropic')
                 
                     ! calculate D matrix, update tmpstress
-                    call ddsdde(lib_isotropic(mat%matkey),dee,PlaneStrain,tmpstrain,tmpstress)
-                    
-                    ! update lcl_stress of this ig point
-                    call update_ig(elem%ig_point(kig),lcl_stress=tmpstress)   
+                    call ddsdde(lib_isotropic(mat%matkey),dee,PlaneStrain,tmpstrain,tmpstress) 
                     
                 case('lamina')
                 
                     ! calculate D matrix, update tmpstress
                     call ddsdde(lib_lamina(mat%matkey),dee,PlaneStrain,tmpstrain,tmpstress)
-                    
-                    ! update lcl_stress of this ig point
-                    call update_ig(elem%ig_point(kig),lcl_stress=tmpstress)
                     
                 case default
                     write(msg_file,*) 'material type not supported for tri element!'
@@ -227,23 +228,43 @@
             end select
             
             ! get D matrix in global coordinates deeg
+            call kdeemat_global(nst,dee,deeg,theta)
             
-            
+            ! calculate B' D B
             beet=transpose(bee)
             btd=matmul(beet,deeg)
             btdb=matmul(btd,bee)
 
+            ! integrate and update K matrix
        		do i=1,ndof
           		do j=1,ndof
-              		elem%K_matrix(i,j) = elem%K_matrix(i,j)+btdb(i,j)*detj*igwt(kig) !-gauss integration
+              		K_matrix(i,j) = K_matrix(i,j)+btdb(i,j)*detj*igwt(kig) !-gauss integration
           		end do
         	end do	
-     	
+            
+            
+            
+            
+            ! update strains to ig point strain array
+            call update_ig(elem%ig_point(kig),strain=tmpstrain)
+            
+            ! update stress of this ig point
+            call update_ig(elem%ig_point(kig),stress=tmpstress)
+            
        	end do !-looped over all int points. ig=nig
         
-        elem%F_vector=matmul(elem%K_matrix,u)   
+        F_vector=matmul(K_matrix,u) 
+        
+        ! update element K matrix and F vector
+        elem%K_matrix=K_matrix
+        elem%F_vector=F_vector
+        
     
     end subroutine integrate_tri_element
+    
+    
+    
+    
     
     
     
@@ -261,14 +282,16 @@
     
     
     
+    
+    
+    
+    
     subroutine empty_ig(ig_point)
         type(tri_ig_point),intent(out) :: ig_point
         
         ig_point%x=zero
-        ig_point%glb_stress=zero
-        ig_point%lcl_stress=zero
-        ig_point%glb_strain=zero
-        ig_point%lcl_strain=zero
+        ig_point%stress=zero
+        ig_point%strain=zero
         
         if(allocated(ig_point%rsdv)) then
             deallocate(ig_point%rsdv)
@@ -285,24 +308,19 @@
     end subroutine empty_ig
   
     
-    subroutine update_ig(ig_point,x,glb_stress,lcl_stress,glb_strain,lcl_strain,rsdv,isdv,lsdv)
+    subroutine update_ig(ig_point,x,stress,strain,rsdv,isdv,lsdv)
         type(tri_ig_point),intent(inout) :: ig_point
         real(kind=dp),optional,intent(in) :: x(:)
-        real(kind=dp),optional,intent(in) :: glb_stress(:),lcl_stress(:)
-        real(kind=dp),optional,intent(in) :: glb_strain(:),lcl_strain(:)
+        real(kind=dp),optional,intent(in) :: stress(:),strain(:)
         real(kind=dp),optional,intent(in) :: rsdv(:)
         integer,optional,intent(in) :: isdv(:)
         logical,optional,intent(in) :: lsdv(:)
         
         if(present(x)) ig_point%x=x
         
-        if(present(glb_stress)) ig_point%glb_stress=glb_stress
+        if(present(stress)) ig_point%stress=stress
         
-        if(present(lcl_stress)) ig_point%lcl_stress=lcl_stress
-        
-        if(present(glb_strain)) ig_point%glb_strain=glb_strain
-        
-        if(present(lcl_strain)) ig_point%lcl_strain=lcl_strain
+        if(present(strain)) ig_point%strain=strain
         
         if(present(rsdv)) then        
             if(allocated(ig_point%rsdv)) then
